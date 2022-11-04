664fa846dd4d6f1518c6338930e40c79794698ed
Benjamin Emde <72041718+BigDiaB@users.noreply.github.com>
2022-11-04 11:39:33 +0100

    Added CLI

--- src/main.c
+++ src/main.c
@@ -4,25 +4,42 @@
 #include <string.h>
 #include "enigma.h"
 
+#define MAX_STR_LEN 256
 
 int main(__attribute__((unused))int argc, __attribute__((unused))char* argv[])
 {
     unsigned int i, len, debug;
-    char* str, *code;
+    char str[MAX_STR_LEN], *code;
+
+    int rotors[3];
+    int offsets[3];
 
     debug = 0;
 
-    str = "GUTEN MORGEN";
+    printf("Rotoren: ");
+    scanf("%d %d %d", &rotors[0],&rotors[1],&rotors[2]);
+
+    printf("Stellungen: ");
+    scanf("%d %d %d", &offsets[0],&offsets[1],&offsets[2]);
+
+    printf("Text: ");
+    scanf("%s", str);
+
     code = malloc((len = strlen(str)) + 1);
     memset(code,0,len +1);
 
-    enigma_init(1,2,3,0,5,0);
+    enigma_init(rotors[0],rotors[1],rotors[2],offsets[0],offsets[1],offsets[2]);
 
     for (i = 0; i < len; i++)
         printf("%c%c",code[i] = enigma(str[i],debug), i == len -1 || debug ? 10 : 0);
 
+    printf("Rotoren: ");
+    scanf("%d %d %d", &rotors[0],&rotors[1],&rotors[2]);
 
-    enigma_init(1,2,3,0,5,0);
+    printf("Stellungen: ");
+    scanf("%d %d %d", &offsets[0],&offsets[1],&offsets[2]);
+
+    enigma_init(rotors[0],rotors[1],rotors[2],offsets[0],offsets[1],offsets[2]);
 
     for (i = 0; i < len; i++)
         printf("%c%c",enigma(code[i],debug), i == len -1 || debug  ? 10 : 0);
@@ -30,4 +47,5 @@ int main(__attribute__((unused))int argc, __attribute__((unused))char* argv[])
     free(code);
 
     exit(0);
+
 }
